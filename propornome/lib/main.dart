fc8553bf2aa6871d6e641d7a5075051b68d8d7ce
Leonardo Cintra <leonardo.ncintra@outlook.com>
2018-03-09 11:01:33 -0300

    louco demais esse flutter

--- propornome/lib/main.dart
+++ propornome/lib/main.dart
@@ -7,18 +7,59 @@ class ProporNome extends StatelessWidget{
   
   @override
   Widget build(BuildContext context){
-    final wordPair = new WordPair.random();
 
     return new MaterialApp(
-      title: 'Bem vindo ao Flutter',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Bem vindo ao Flutter'),
-        ),
-        body: new Center(
-          child: new Text(wordPair.asPascalCase),
+      title: 'Startup Gerador de Nome',
+      home: new PalavraAleatoria()
+    );
+  }
+}
+
+class PalavraAleatoria extends StatefulWidget{
+  @override
+  createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<PalavraAleatoria>{
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) {
+          return new Divider();
+        }
+
+        final index = i ~/ 2;
+        if (index >= _suggestions.length){
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+
+        return _buildRow(_suggestions[index]);
+      }
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return new ListTile(
+      title: new ListTile(
+        title: new Text(
+          pair.asPascalCase,
+          style: _biggerFont,
         ),
       ),
+    );
+  }
+
+  @override
+  Widget build (BuildContext context){
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Iniciar nome do gerador'),
+      ),
+      body: _buildSuggestions(),
     );
   }
 }
