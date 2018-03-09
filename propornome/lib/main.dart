7ba9373622a66cc78c3b840e35e683e34b761f6d
Leonardo Cintra <leonardo.ncintra@outlook.com>
2018-03-09 10:21:56 -0300

    english pair

--- propornome/lib/main.dart
+++ propornome/lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(new ProporNome());
 
 class ProporNome extends StatelessWidget{
+  
   @override
   Widget build(BuildContext context){
+    final wordPair = new WordPair.random();
+
     return new MaterialApp(
       title: 'Bem vindo ao Flutter',
       home: new Scaffold(
@@ -12,10 +16,9 @@ class ProporNome extends StatelessWidget{
           title: new Text('Bem vindo ao Flutter'),
         ),
         body: new Center(
-          child: new Text('Ola Mundo!'),
+          child: new Text(wordPair.asPascalCase),
         ),
       ),
     );
   }
 }
-
