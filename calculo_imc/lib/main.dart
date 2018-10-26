5ae58228aa7d7ce8b70805d59009ea8f2e289ec8
Leonardo Cintra <leonardo.ncintra@outlook.com>
2018-10-26 13:22:55 -0300

    add campos texto

--- calculo_imc/lib/main.dart
+++ calculo_imc/lib/main.dart
@@ -26,6 +26,33 @@ class _HomeState extends State<Home> {
           )
         ],
       ),
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Icon(
+            Icons.person_outline,
+            size: 120.0,
+            color: Colors.green,
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+                labelText: "Peso (Kg)",
+                labelStyle: TextStyle(color: Colors.green)),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.green, fontSize: 25.0),
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+                labelText: "Altura (cm)",
+                labelStyle: TextStyle(color: Colors.green)),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.green, fontSize: 25.0),
+          )
+        ],
+      ),
     );
   }
 }
