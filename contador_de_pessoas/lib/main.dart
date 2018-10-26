7c743830aef0c7a90a0847a39fa686e34b9ace20
Leonardo Cintra <leonardo.ncintra@outlook.com>
2018-10-26 12:45:00 -0300

    add images padding

--- contador_de_pessoas/lib/main.dart
+++ contador_de_pessoas/lib/main.dart
@@ -3,38 +3,51 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
       title: "Contador de pessoas",
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      home: Stack(
         children: <Widget>[
-          Text(
-            "Pessoas: 0",
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-          ),
-          Row(
+          Image.asset("images/restaurant.jpg",
+              fit: BoxFit.cover, height: 1000.0),
+          Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              FlatButton(
-                child: Text(
-                  "-1",
-                  style: TextStyle(fontSize: 40.0, color: Colors.white),
-                ),
-                onPressed: () {},
+              Text(
+                "Pessoas: 0",
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
               ),
-              FlatButton(
-                child: Text(
-                  "+1",
-                  style: TextStyle(fontSize: 40.0, color: Colors.white),
-                ),
-                onPressed: () {},
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      child: Text(
+                        "+1",
+                        style: TextStyle(fontSize: 40.0, color: Colors.white),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      child: Text(
+                        "-1",
+                        style: TextStyle(fontSize: 40.0, color: Colors.white),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
+              ),
+              Text(
+                "Pode entrar!",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 30.0),
               )
             ],
-          ),
-          Text(
-            "Pode entrar!",
-            style: TextStyle(
-                color: Colors.white,
-                fontStyle: FontStyle.italic,
-                fontSize: 30.0),
           )
         ],
       )));
