1d717f89871f28f28929a875211d78e1362885b7
Leonardo Cintra <leonardo.ncintra@outlook.com>
2018-11-06 17:27:22 -0200

    Intial project #sqn

---
+++ pokemon/lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'pokemon.dart';
+
+void main() => runApp(MaterialApp(
+      title: "Poke App",
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    ));
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var url =
+      "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
+
+  PokeHub pokeHub;
+
+  @override
+  void initState() {
+    super.initState();
+
+    fetchData();
+  }
+
+  fetchData() async {
+    var res = await http.get(url);
+    var decodedJson = jsonDecode(res.body);
+
+    pokeHub = PokeHub.fromJson(decodedJson);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Poke App"), backgroundColor: Colors.cyan),
+      body: GridView.count(
+          crossAxisCount: 2,
+          children: pokeHub.pokemon
+              .map((poke) => Padding(
+                    padding: const EdgeInsets.all(2.0),
+                    child: Card(
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            height: 100.0,
+                            width: 100.0,
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image: NetworkImage(poke.img))),
+                          ),
+                          Text(
+                            poke.name,
+                            style: TextStyle(
+                                fontSize: 20.0, fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                    ),
+                  ))
+              .toList()),
+      drawer: Drawer(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.cyan,
+        child: Icon(Icons.refresh),
+      ),
+    );
+  }
+}
