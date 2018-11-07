40256a90bb214fc7cddba3bd5bcfb51db725c9f5
Leonardo Cintra <leonardo.ncintra@outlook.com>
2018-11-07 13:02:59 -0200

    finalizado

--- pokemon/lib/main.dart
+++ pokemon/lib/main.dart
@@ -1,8 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:pokemon/pokemondetail.dart';
-import 'dart:convert';
-import 'pokemon.dart';
+import 'package:pokemon/pokemon.dart';
 
 void main() => runApp(MaterialApp(
       title: "Poke App",
@@ -12,10 +13,12 @@ void main() => runApp(MaterialApp(
 
 class HomePage extends StatefulWidget {
   @override
-  _HomePageState createState() => _HomePageState();
+  HomePageState createState() {
+    return new HomePageState();
+  }
 }
 
-class _HomePageState extends State<HomePage> {
+class HomePageState extends State<HomePage> {
   var url =
       "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
 
@@ -31,15 +34,18 @@ class _HomePageState extends State<HomePage> {
   fetchData() async {
     var res = await http.get(url);
     var decodedJson = jsonDecode(res.body);
-
     pokeHub = PokeHub.fromJson(decodedJson);
+    print(pokeHub.toJson());
     setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Poke App"), backgroundColor: Colors.cyan),
+      appBar: AppBar(
+        title: Text("Poke App"),
+        backgroundColor: Colors.cyan,
+      ),
       body: pokeHub == null
           ? Center(
               child: CircularProgressIndicator(),
@@ -58,30 +64,35 @@ class _HomePageState extends State<HomePage> {
                                           pokemon: poke,
                                         )));
                           },
-                          child: Card(
-                            elevation: 3.0,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: <Widget>[
-                                Container(
-                                  height: 100.0,
-                                  width: 100.0,
-                                  decoration: BoxDecoration(
-                                      image: DecorationImage(
-                                          image: NetworkImage(poke.img))),
-                                ),
-                                Text(
-                                  poke.name,
-                                  style: TextStyle(
+                          child: Hero(
+                            tag: poke.img,
+                            child: Card(
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: <Widget>[
+                                  Container(
+                                    height: 100.0,
+                                    width: 100.0,
+                                    decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                            image: NetworkImage(poke.img))),
+                                  ),
+                                  Text(
+                                    poke.name,
+                                    style: TextStyle(
                                       fontSize: 20.0,
-                                      fontWeight: FontWeight.bold),
-                                )
-                              ],
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  )
+                                ],
+                              ),
                             ),
                           ),
                         ),
                       ))
-                  .toList()),
+                  .toList(),
+            ),
       drawer: Drawer(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
