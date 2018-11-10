29036af68bc6c34f60fc57308df90ad4d8d7a0ca
Leonardo Cintra <leonardo.ncintra@outlook.com>
2018-11-10 13:30:51 -0200

    initial do dart

--- agenda_contatos/lib/pages/home_page.dart
+++ agenda_contatos/lib/pages/home_page.dart
@@ -9,25 +9,38 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   ContactHelper helper = ContactHelper();
 
+  List<Contact> contacts = List();
+
   @override
   void initState() {
     super.initState();
 
-    Contact contact = Contact();
-    contact.name = "Juliana Cintra";
-    contact.email = "juliana.cintra@luizalabs.com";
-    contact.phone = "45454545";
-    contact.img = "teste";
-
-    helper.saveContact(contact);
-
     helper.getAllContact().then((list) {
-      print(list);
+      setState(() {
+        contacts = list;
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Contatos"),
+        backgroundColor: Colors.red,
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.red,
+      ),
+      body: ListView.builder(
+        padding: EdgeInsets.all(10.0),
+        itemCount: contacts.length,
+        itemBuilder: (context, index) {},
+      ),
+    );
   }
 }
