8e930b1e33843253012b0473394f2475bb54311c
Leonardo Cintra <leonardo.ncintra@outlook.com>
2019-01-24 13:14:46 -0200

    criado botao de enviar

--- chat_online/lib/main.dart
+++ chat_online/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -72,6 +73,8 @@ class TextComposer extends StatefulWidget {
 }
 
 class _TextComposerState extends State<TextComposer> {
+  bool _isComposing = false;
+
   @override
   Widget build(BuildContext context) {
     return IconTheme(
@@ -94,8 +97,23 @@ class _TextComposerState extends State<TextComposer> {
               child: TextField(
                 decoration:
                     InputDecoration.collapsed(hintText: "Enviar mensagem"),
+                onChanged: (text) {
+                  setState(() {
+                    _isComposing = text.length > 0;
+                  });
+                },
               ),
-            )
+            ),
+            Container(
+                margin: const EdgeInsets.symmetric(horizontal: 4.0),
+                child: Theme.of(context).platform == TargetPlatform.iOS
+                    ? CupertinoButton(
+                        child: Text("Enviar"),
+                        onPressed: _isComposing ? () {} : null)
+                    : IconButton(
+                        icon: Icon(Icons.send),
+                        onPressed: _isComposing ? () {} : null,
+                      )),
           ],
         ),
       ),
