77b6111180eea6bb6c788276d9f736abadfd709a
Leonardo Cintra <leonardo.ncintra@outlook.com>
2019-02-05 12:38:15 -0200

    page controller

--- lojinha/lib/screens/home_screen.dart
+++ lojinha/lib/screens/home_screen.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
+  final _pageController = PageController();
+
   @override
   Widget build(BuildContext context) {
     return PageView(
+      controller: _pageController,
+      physics: NeverScrollableScrollPhysics(),
       children: <Widget>[
-        Container(color: Colors.yellow,),
-        Container(color: Colors.red,)
+        Container(
+          color: Colors.yellow,
+        ),
+        Container(
+          color: Colors.red,
+        )
       ],
     );
   }
